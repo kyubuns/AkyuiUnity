5fd236f17f4ca92565cbf8fe46050428d1b235ba
kyubuns <kyubuns@gmail.com>
2021-01-08 23:16:49 +0900

    feat: zip解凍してjson読めるところまで

--- Assets/AkyuiUnity/Editor/Importer.cs
+++ Assets/AkyuiUnity/Editor/Importer.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
+using System.IO;
+using AkyuiUnity.Editor.MiniJSON;
 using UnityEngine;
+using ICSharpCode.SharpZipLib.Zip;
 
 namespace AkyuiUnity.Editor
 {
@@ -8,7 +12,28 @@ namespace AkyuiUnity.Editor
         {
             foreach (var filePath in filePaths)
             {
-                Debug.Log($"Import: {filePath}");
+                using (var zipFile = new ZipFile(filePath))
+                {
+                    var layoutJson = GetJson(zipFile, "layout.json");
+                    foreach (var key in layoutJson.Keys)
+                    {
+                        Debug.Log($"{key} = {layoutJson[key]}");
+                    }
+                }
+            }
+        }
+
+        private static Dictionary<string, object> GetJson(ZipFile zipFile, string name)
+        {
+            var findPath = Path.Combine(Path.GetFileNameWithoutExtension(zipFile.Name), name);
+            var layoutJson = zipFile.FindEntry(findPath, true);
+
+            var stream = zipFile.GetInputStream(layoutJson);
+            using (var reader = new StreamReader(stream))
+            {
+                var jsonString = reader.ReadToEnd();
+                var json = (Dictionary<string, object>) Json.Deserialize(jsonString);
+                return json;
             }
         }
     }
