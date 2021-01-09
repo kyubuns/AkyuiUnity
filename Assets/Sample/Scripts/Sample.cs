ae2f20c0fdf92cf879487332953555881f0a9c60
kyubuns <kyubuns@gmail.com>
2021-01-09 18:47:24 +0900

    feat: SampleScript

---
+++ Assets/Sample/Scripts/Sample.cs
@@ -0,0 +1,37 @@
+﻿using AnKuchen.Map;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AkyuiUnity.Sample
+{
+    public class Sample : MonoBehaviour
+    {
+        [SerializeField] private UICache uiCache = default;
+
+        public void Start()
+        {
+            var ui = new UiElements(uiCache);
+        }
+
+        public class UiElements : IMappedObject
+        {
+            public IMapper Mapper { get; private set; }
+            public GameObject Root { get; private set; }
+
+            public UiElements()
+            {
+            }
+
+            public UiElements(IMapper mapper)
+            {
+                Initialize(mapper);
+            }
+
+            public void Initialize(IMapper mapper)
+            {
+                Mapper = mapper;
+                Root = mapper.Get();
+            }
+        }
+    }
+}
