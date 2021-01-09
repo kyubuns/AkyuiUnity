cfda9ff66e9221ac70a58a4df6ebc29c53782837
kyubuns <kyubuns@gmail.com>
2021-01-09 18:36:06 +0900

    feat: AnKuchenTrigger

---
+++ Assets/AkyuiUnity.AnKuchen/Scripts/AnKuchenTrigger.cs
@@ -0,0 +1,16 @@
+﻿using AkyuiUnity.Editor.ScriptableObject;
+using AnKuchen.Map;
+using UnityEngine;
+
+namespace AkyuiUnity.AnKuchen
+{
+    [CreateAssetMenu(menuName = "Akyui/AnKuchenTrigger")]
+    public class AnKuchenTrigger : AkyuiImportTrigger
+    {
+        public override void OnPostprocessPrefab(GameObject prefab)
+        {
+            var uiCache = prefab.AddComponent<UICache>();
+            uiCache.CreateCache();
+        }
+    }
+}
