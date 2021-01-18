4eddb0692a70c95daa64c433400f9d0e2484c460
kyubuns <kyubuns@gmail.com>
2021-01-18 18:42:41 +0900

    fix: Scroll Position

--- Assets/AkyuiUnity.Xd/Scripts/XdGroupParser/ScrollGroupParser.cs
+++ Assets/AkyuiUnity.Xd/Scripts/XdGroupParser/ScrollGroupParser.cs
@@ -15,26 +15,37 @@ namespace AkyuiUnity.Xd
         {
             var scrollingType = xdObject?.Meta?.Ux?.ScrollingType;
 
-            if (scrollingType == "horizontal")
+            if (scrollingType == "vertical")
             {
-                var viewportWidth = xdObject?.Meta?.Ux?.ViewportWidth ?? 0f;
-                var offsetX = xdObject?.Meta?.Ux?.OffsetX ?? 0f;
-                return new Rect(position.x + offsetX, position.y, viewportWidth, rect.height);
+                var offsetY = xdObject?.Meta?.Ux?.OffsetY ?? 0f;
+                var viewportHeight = xdObject?.Meta?.Ux?.ViewportHeight ?? 0f;
+                return new Rect(rect.position.x, position.y + offsetY, rect.size.x, viewportHeight);
             }
-
-            var viewportHeight = xdObject?.Meta?.Ux?.ViewportHeight ?? 0f;
-            var offsetY = xdObject?.Meta?.Ux?.OffsetY ?? 0f;
-            return new Rect(position.x, position.y + offsetY, rect.width, viewportHeight);
+            else
+            {
+                var offsetX = xdObject?.Meta?.Ux?.OffsetX ?? 0f;
+                var viewportWidth = xdObject?.Meta?.Ux?.ViewportWidth ?? 0f;
+                return new Rect(position.x + offsetX, rect.position.y, viewportWidth, rect.size.y);
+            }
         }
 
         public IComponent[] Render(XdObjectJson instanceObject, XdObjectJson symbolObject, ref XdObjectJson[] children)
         {
             var spacing = 0f;
 
+            var scrollingType = instanceObject?.Meta?.Ux?.ScrollingType;
             if (children.Length == 1 && RepeatGridGroupParser.Is(children[0], null))
             {
                 var repeatGrid = children[0];
-                spacing = repeatGrid.Meta?.Ux?.RepeatGrid?.PaddingY ?? 0f;
+                if (scrollingType == "vertical")
+                {
+                    spacing = repeatGrid.Meta?.Ux?.RepeatGrid?.PaddingY ?? 0f;
+                }
+                else
+                {
+                    spacing = repeatGrid.Meta?.Ux?.RepeatGrid?.PaddingX ?? 0f;
+                }
+
                 children = new[] { repeatGrid.Group.Children[0].Group.Children[0] };
             }
 
