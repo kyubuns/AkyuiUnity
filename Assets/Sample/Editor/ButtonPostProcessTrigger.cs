d15e1ebe1d10fb5404d48121b0332165eaa93efb
kyubuns <kyubuns@gmail.com>
2021-01-18 18:51:58 +0900

    feat: ButtonPostProcessTrigger

---
+++ Assets/Sample/Editor/ButtonPostProcessTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using AkyuiUnity.Editor.ScriptableObject;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AkyuiUnity.Sample
+{
+    [CreateAssetMenu(menuName = "AkyuiSample/ButtonPostProcessTrigger")]
+    public class ButtonPostProcessTrigger : AkyuiImportTrigger
+    {
+        public override void OnPostprocessPrefab(ref GameObject prefab, ref GameObjectWithId[] idAndGameObjects)
+        {
+            foreach (var button in prefab.GetComponentsInChildren<Button>())
+            {
+                var image = button.GetComponentsInChildren<Image>()
+                    .Where(x => x.color != Color.clear)
+                    .ToArray();
+
+                if (image.Length == 1)
+                {
+                    button.targetGraphic = image.First();
+                }
+            }
+        }
+    }
+}
