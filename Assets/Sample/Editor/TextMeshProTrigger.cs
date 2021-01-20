dca2c7a0b664158fb92211b6f694644edf004568
kyubuns <kyubuns@gmail.com>
2021-01-20 15:46:18 +0900

    fix: TextMeshPro

--- Assets/Sample/Editor/TextMeshProTrigger.cs
+++ Assets/Sample/Editor/TextMeshProTrigger.cs
@@ -14,65 +14,67 @@ namespace AkyuiUnity.CommonTrigger
 
         public override Component SetOrCreateComponentValue(GameObject gameObject, TargetComponentGetter componentGetter, IComponent component, IAssetLoader assetLoader)
         {
-            if (component is TextComponent textComponent)
+            if (!(component is TextComponent textComponent)) return null;
+
+            var text = componentGetter.GetComponent<TextMeshProUGUI>();
+            text.enableWordWrapping = false;
+            text.overflowMode = TextOverflowModes.Overflow;
+
+            if (textComponent.Text != null) text.text = textComponent.Text;
+            if (textComponent.Size != null) text.fontSize = Mathf.RoundToInt(textComponent.Size.Value);
+            if (textComponent.Color != null) text.color = textComponent.Color.Value;
+            if (textComponent.Align != null)
             {
-                var text = componentGetter.GetComponent<TextMeshProUGUI>();
-                text.enableWordWrapping = false;
-                text.overflowMode = TextOverflowModes.Overflow;
-
-                if (textComponent.Text != null) text.text = textComponent.Text;
-                if (textComponent.Size != null) text.fontSize = Mathf.RoundToInt(textComponent.Size.Value);
-                if (textComponent.Color != null) text.color = textComponent.Color.Value;
-                if (textComponent.Align != null)
+                switch (textComponent.Align.Value)
                 {
-                    switch (textComponent.Align.Value)
-                    {
-                        case TextComponent.TextAlign.UpperLeft:
-                            text.alignment = TextAlignmentOptions.TopLeft;
-                            break;
-                        case TextComponent.TextAlign.UpperCenter:
-                            text.alignment = TextAlignmentOptions.Top;
-                            break;
-                        case TextComponent.TextAlign.UpperRight:
-                            text.alignment = TextAlignmentOptions.TopRight;
-                            break;
-                        case TextComponent.TextAlign.MiddleLeft:
-                            text.alignment = TextAlignmentOptions.MidlineLeft;
-                            break;
-                        case TextComponent.TextAlign.MiddleCenter:
-                            text.alignment = TextAlignmentOptions.Midline;
-                            break;
-                        case TextComponent.TextAlign.MiddleRight:
-                            text.alignment = TextAlignmentOptions.MidlineRight;
-                            break;
-                        case TextComponent.TextAlign.LowerLeft:
-                            text.alignment = TextAlignmentOptions.BottomLeft;
-                            break;
-                        case TextComponent.TextAlign.LowerCenter:
-                            text.alignment = TextAlignmentOptions.Bottom;
-                            break;
-                        case TextComponent.TextAlign.LowerRight:
-                            text.alignment = TextAlignmentOptions.BottomRight;
-                            break;
-                        default:
-                            throw new ArgumentOutOfRangeException();
-                    }
+                    case TextComponent.TextAlign.UpperLeft:
+                        text.alignment = TextAlignmentOptions.TopLeft;
+                        break;
+                    case TextComponent.TextAlign.UpperCenter:
+                        text.alignment = TextAlignmentOptions.Top;
+                        break;
+                    case TextComponent.TextAlign.UpperRight:
+                        text.alignment = TextAlignmentOptions.TopRight;
+                        break;
+                    case TextComponent.TextAlign.MiddleLeft:
+                        text.alignment = TextAlignmentOptions.MidlineLeft;
+                        break;
+                    case TextComponent.TextAlign.MiddleCenter:
+                        text.alignment = TextAlignmentOptions.Midline;
+                        break;
+                    case TextComponent.TextAlign.MiddleRight:
+                        text.alignment = TextAlignmentOptions.MidlineRight;
+                        break;
+                    case TextComponent.TextAlign.LowerLeft:
+                        text.alignment = TextAlignmentOptions.BottomLeft;
+                        break;
+                    case TextComponent.TextAlign.LowerCenter:
+                        text.alignment = TextAlignmentOptions.Bottom;
+                        break;
+                    case TextComponent.TextAlign.LowerRight:
+                        text.alignment = TextAlignmentOptions.BottomRight;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
                 }
-
-                if (textComponent.Font != null)
-                {
-                    var fontPath = fontFilePath.Replace("{name}", textComponent.Font) + ".asset";
-                    text.font = AssetDatabase.LoadAssetAtPath<TMP_FontAsset>(fontPath);
-                    if (text.font == null)
-                    {
-                        Debug.LogWarning($"TextMeshPro Font {fontPath} is not found");
-                    }
-                }
-
-                return text;
             }
 
-            return null;
+            if (textComponent.Font != null)
+            {
+                var fontPath = fontFilePath.Replace("{name}", textComponent.Font) + ".asset";
+                var loadFont = AssetDatabase.LoadAssetAtPath<TMP_FontAsset>(fontPath);
+                if (loadFont != null)
+                {
+                    text.font = loadFont;
+                }
+                else
+                {
+                    Debug.LogWarning($"TextMeshPro Font {fontPath} is not found");
+                }
+            }
+
+            return text;
+
         }
     }
 }
