886f894eda576ef2d2c2cbcaae4d15bc6218abd1
kyubuns <kyubuns@gmail.com>
2021-01-21 21:00:46 +0900

    [Xd] feat: Settingsの表示順が気に入らなかったので順番変更

--- Assets/AkyuiUnity.Xd/Scripts/XdImportSettings.cs
+++ Assets/AkyuiUnity.Xd/Scripts/XdImportSettings.cs
@@ -6,13 +6,13 @@ namespace AkyuiUnity.Xd
     [CreateAssetMenu(menuName = "Akyui/XdImportSettings", fileName = "XdImportSettings")]
     public class XdImportSettings : AkyuiImportSettings
     {
-        public string AkyuiOutputPath => akyuiOutputPath;
-        [SerializeField] private string akyuiOutputPath = "";
-
         public AkyuiXdObjectParser[] ObjectParsers => objectParsers;
         [SerializeField] private AkyuiXdObjectParser[] objectParsers = default;
 
         public AkyuiXdGroupParser[] GroupParsers => groupParsers;
         [SerializeField] private AkyuiXdGroupParser[] groupParsers = default;
+
+        public string AkyuiOutputPath => akyuiOutputPath;
+        [SerializeField] private string akyuiOutputPath = "";
     }
 }
