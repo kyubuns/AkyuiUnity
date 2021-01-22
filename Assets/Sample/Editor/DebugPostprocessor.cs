f1b19a4cb4e2e3e49b0cf9a1fa80e359863ff144
kyubuns <kyubuns@gmail.com>
2021-01-22 18:57:37 +0900

    [Dev] feat: DebugPostprocessor

---
+++ Assets/Sample/Editor/DebugPostprocessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AkyuiUnity.Editor.ScriptableObject;
+using UnityEditor;
+using UnityEngine;
+
+namespace AkyuiUnity.Sample
+{
+    public class PostProcessImportAsset : AssetPostprocessor
+    {
+        public static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            foreach (var importedAsset in importedAssets) Debug.Log($"[Debug] importedAsset {importedAsset}");
+            foreach (var deletedAsset in deletedAssets) Debug.Log($"[Debug] deletedAsset {deletedAsset}");
+            foreach (var movedAsset in movedAssets) Debug.Log($"[Debug] movedAsset {movedAsset}");
+            foreach (var movedFromAssetPath in movedFromAssetPaths) Debug.Log($"[Debug] movedFromAssetPath {movedFromAssetPath}");
+        }
+    }
+}
