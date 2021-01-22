7ebbe9d3e12cb33096ec1547521f53d4b79ce7f8
kyubuns <kyubuns@gmail.com>
2021-01-22 12:53:34 +0900

    [Xd] feat: XdCommentOutTrigger

---
+++ Assets/AkyuiUnity.Xd/CommonTrigger/XdCommentOutTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using XdParser.Internal;
+
+namespace AkyuiUnity.Xd.CommonTrigger
+{
+    [CreateAssetMenu(menuName = "AkyuiXd/Triggers/XdCommentOut", fileName = nameof(XdCommentOutTrigger))]
+    public class XdCommentOutTrigger : AkyuiXdImportTrigger
+    {
+        [SerializeField] private string startsWith = "#";
+
+        public override XdObjectJson OnCreateXdObject(XdObjectJson xdObject)
+        {
+            var xdObjectName = xdObject?.Name ?? "";
+            Debug.Log($"Trigger! {xdObjectName}");
+            if (xdObjectName.StartsWith(startsWith)) return null;
+
+            return xdObject;
+        }
+    }
+}
