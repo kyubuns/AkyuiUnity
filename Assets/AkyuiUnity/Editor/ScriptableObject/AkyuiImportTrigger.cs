c8f30b9c7d05f7419d47937f47a5953ce0c7bae0
kyubuns <kyubuns@gmail.com>
2021-01-29 14:51:11 +0900

    [Akyui] refactor: Callbackをだいたい呼ばれる順番に並べる

--- Assets/AkyuiUnity/Editor/ScriptableObject/AkyuiImportTrigger.cs
+++ Assets/AkyuiUnity/Editor/ScriptableObject/AkyuiImportTrigger.cs
@@ -12,45 +12,25 @@ namespace AkyuiUnity.Editor.ScriptableObject
         void OnPreprocessAllAssets(IAkyuiLoader loader, ref List<IAsset> assets);
         void OnPreprocessAsset(IAkyuiLoader loader, ref byte[] bytes, ref IAsset asset, ref Dictionary<string, object> userData);
         void OnUnityPreprocessAsset(AssetImporter assetImporter, IAsset asset, ref Dictionary<string, object> userData);
-        void OnPostprocessPrefab(IAkyuiLoader loader, ref GameObject prefab);
         void OnPostprocessAllAssets(IAkyuiLoader loader, Object[] importAssets);
+
         string OnLoadAsset(string fileName);
+
+        void OnPostprocessPrefab(IAkyuiLoader loader, ref GameObject prefab);
     }
 
     public abstract class AkyuiImportTrigger : UnityEngine.ScriptableObject, IAkyuiImportTrigger
     {
-        public virtual void OnPreprocessAllAssets(IAkyuiLoader loader, ref List<IAsset> assets)
-        {
-        }
+        public virtual void OnPreprocessAllAssets(IAkyuiLoader loader, ref List<IAsset> assets) { }
+        public virtual void OnPreprocessAsset(IAkyuiLoader loader, ref byte[] bytes, ref IAsset asset, ref Dictionary<string, object> userData) { }
+        public virtual void OnUnityPreprocessAsset(AssetImporter assetImporter, IAsset asset, ref Dictionary<string, object> userData) { }
+        public virtual void OnPostprocessAllAssets(IAkyuiLoader loader, Object[] importAssets) { }
 
-        public virtual void OnPreprocessAsset(IAkyuiLoader loader, ref byte[] bytes, ref IAsset asset, ref Dictionary<string, object> userData)
-        {
-        }
+        public virtual Component CreateComponent(GameObject gameObject, IComponent component, IAssetLoader assetLoader) => null;
+        public virtual void OnPostprocessComponent(GameObject gameObject, IComponent component) { }
 
-        public virtual void OnUnityPreprocessAsset(AssetImporter assetImporter, IAsset asset, ref Dictionary<string, object> userData)
-        {
-        }
+        public virtual string OnLoadAsset(string fileName) => null;
 
-        public virtual void OnPostprocessPrefab(IAkyuiLoader loader, ref GameObject prefab)
-        {
-        }
-
-        public virtual void OnPostprocessAllAssets(IAkyuiLoader loader, Object[] importAssets)
-        {
-        }
-
-        public virtual Component CreateComponent(GameObject gameObject, IComponent component, IAssetLoader assetLoader)
-        {
-            return null;
-        }
-
-        public virtual void OnPostprocessComponent(GameObject gameObject, IComponent component)
-        {
-        }
-
-        public virtual string OnLoadAsset(string fileName)
-        {
-            return null;
-        }
+        public virtual void OnPostprocessPrefab(IAkyuiLoader loader, ref GameObject prefab) { }
     }
 }
