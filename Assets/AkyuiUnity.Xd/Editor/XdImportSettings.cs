485fe3e2579d8d79935f6d78b104d9c09eecb791
kyubuns <kyubuns@gmail.com>
2021-02-11 17:02:28 +0900

    [Xd] fix: デフォルト設定でXdImportSettingsを使うとコケるのを修正

--- Assets/AkyuiUnity.Xd/Editor/XdImportSettings.cs
+++ Assets/AkyuiUnity.Xd/Editor/XdImportSettings.cs
@@ -7,13 +7,13 @@ namespace AkyuiUnity.Xd
     [CreateAssetMenu(menuName = "AkyuiXd/XdImportSettings", fileName = "XdImportSettings")]
     public class XdImportSettings : AkyuiImportSettings
     {
-        public AkyuiXdObjectParser[] ObjectParsers => objectParsers.Where(x => x != null).ToArray() ?? new AkyuiXdObjectParser[] { };
+        public AkyuiXdObjectParser[] ObjectParsers => objectParsers?.Where(x => x != null).ToArray() ?? new AkyuiXdObjectParser[] { };
         [SerializeField] private AkyuiXdObjectParser[] objectParsers = default;
 
-        public AkyuiXdGroupParser[] GroupParsers => groupParsers.Where(x => x != null).ToArray() ?? new AkyuiXdGroupParser[] { };
+        public AkyuiXdGroupParser[] GroupParsers => groupParsers?.Where(x => x != null).ToArray() ?? new AkyuiXdGroupParser[] { };
         [SerializeField] private AkyuiXdGroupParser[] groupParsers = default;
 
-        public AkyuiXdImportTrigger[] XdTriggers => xdTriggers.Where(x => x != null).ToArray() ?? new AkyuiXdImportTrigger[] { };
+        public AkyuiXdImportTrigger[] XdTriggers => xdTriggers?.Where(x => x != null).ToArray() ?? new AkyuiXdImportTrigger[] { };
         [SerializeField] private AkyuiXdImportTrigger[] xdTriggers = default;
 
         public string AkyuiOutputPath => akyuiOutputPath;
