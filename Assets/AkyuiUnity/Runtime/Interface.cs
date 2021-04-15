1bc05aee4733e7a4ffb8e2142ea042724b23435f
kyubuns <kyubuns@gmail.com>
2021-04-15 11:08:59 +0900

    [Akyui] 1.1.6

--- Assets/AkyuiUnity/Runtime/Interface.cs
+++ Assets/AkyuiUnity/Runtime/Interface.cs
@@ -6,7 +6,7 @@ namespace AkyuiUnity
 {
     public static class Const
     {
-        public const string AkyuiVersion = "1.1.3";
+        public const string AkyuiVersion = "1.1.6";
     }
 
     public class LayoutInfo
