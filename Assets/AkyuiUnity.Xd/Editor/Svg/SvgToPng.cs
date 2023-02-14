72b5c2396ed64b94bc431a4a444aeb5ce8888ffc
kyubuns <kyubuns@gmail.com>
2023-02-14 11:09:29 +0900

    delete debug log

--- Assets/AkyuiUnity.Xd/Editor/Svg/SvgToPng.cs
+++ Assets/AkyuiUnity.Xd/Editor/Svg/SvgToPng.cs
@@ -49,7 +49,6 @@ namespace AkyuiUnity.Xd
             }))
             {
                 File.WriteAllBytes(saveFullPath, System.Text.Encoding.UTF8.GetBytes(svg));
-                Debug.Log(svg);
                 AssetDatabase.ImportAsset(savePath);
                 // SvgImportTrigger.OnPreprocessAssetが呼ばれる
                 var texture = AssetDatabase.LoadAssetAtPath<Texture2D>(savePath);
